027e5071935501860e150b6409d8eb97e59a0021
MD Jackwan <92715067+GamerGiri@users.noreply.github.com>
2023-09-23 20:17:44 +0600

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,11 +11,11 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv("API_ID", "25092262"))
     API_HASH = str(getenv("API_HASH", "6154a0b7f5c3e8605640d3d5f8093e78"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN","6171238258:AAEYOhFc9lfpZFoIgewvOWeZ4sSBbxBTrzE"))
-    name = str(getenv("name", "moneycasefilestream"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN","5975111558:AAH6SpM6YBoGoLiOsaPDgftI1FPE9SPf3U0"))
+    name = str(getenv("name", "l4mstream")
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001748699071"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001915620273"))
     PORT = int(getenv("PORT", 8080))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -25,21 +25,21 @@ class Var(object):
     OWNER_USERNAME = str(getenv("OWNER_USERNAME", "Infokeeda"))
     if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv("APP_NAME", "moneycasefilestream"))
+        APP_NAME = str(getenv("APP_NAME", "l4mstream"))
 
     else:
         ON_HEROKU = False
     FQDN = (
         str(getenv("FQDN", BIND_ADRESS))
         if not ON_HEROKU or getenv("FQDN")
-        else f"vivdisk.pro"
+        else f"streaam.streaam.in"
     )
     HAS_SSL = bool(getenv("HAS_SSL", False))
-    URL = f"https://vivdisk.pro/" if HAS_SSL else f"http://vivdisk.pro/"
+    URL = f"https://streaam.streaam.in/" if HAS_SSL else f"http://streaam.streaam.in/"
     DATABASE_URL = str(getenv("DATABASE_URL", "mongodb://mongo:VPSMongoMoney@91.188.254.30:1234/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv("UPDATES_CHANNEL", None))
     BANNED_CHANNELS = list(
-        {int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()}
+        {int(x) for x in str(getenv("BANNED_CHANNELS", "-1001915620277")).split()}
     )
-    BOT_USERNAME = str(getenv("BOT_USERNAME", "moneycase_filestream_bot"))
-    FILE_STORE_BOT_USERNAME = str(getenv("FILE_STORE_BOT_USERNAME", "moneycase_store_file_bot"))
+    BOT_USERNAME = str(getenv("BOT_USERNAME", "l4mstream_bot"))
+    FILE_STORE_BOT_USERNAME = str(getenv("FILE_STORE_BOT_USERNAME", "l4m_destination_bot"))
